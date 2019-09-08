f25483fad1e2b04a1c5b2dfccff972ce72e208df
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-09-07 20:02:28 -0700

    Add files via upload

---
+++ 001_syntax/001_instruction.py
@@ -0,0 +1,29 @@
+# script mode / batch mode
+'''
+---> Create a new file
+---> write the python statements  on  untilted file
+---> save the file as .py extention
+---> run the python direclty
+'''
+
+print "hell\
+o"  # single statement
+print "hai" ; print "sai"; print "bye"  # multiple statement
+#multiline statement
+print [1+2+
+       3+4+
+       5]
+print (1+2+
+       3+4+
+       5)
+print {1+2+
+       3+4}
+print 1+2+\
+      3+4+\
+      5
+print'''
+hello
+hai
+bye
+'''
+print'''hello'''
---
+++ 001_syntax/002_compound.py
@@ -0,0 +1,16 @@
+#compound statement
+
+'''
+function
+
+if
+else
+for
+while
+exceptions
+'''
+if(True):
+    print "hello"
+    print"hai"
+    print"hai"
+    
