da8c545f977c3387706d3694de1f37d2e2e347f6
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-09-08 20:09:20 -0700

    Add files via upload

---
+++ 001_syntax/003_fundamental.py
@@ -0,0 +1,29 @@
+print "*************  int *************"
+var = 123
+print "var : ",var
+print type(var)
+print id(var)
+print "*************  float *************"
+var = 12.5653
+print "var : ",var
+print type(var)
+print id(var)
+print "*************  complex*************"
+var = 12 + 45j
+print "var : ",var
+print type(var)
+print id(var)
+print "*************  bool *************"
+var = True
+print "var : ",var
+print type(var)
+print id(var)
+var = False
+print "var : ",var
+print type(var)
+print id(var)
+print "*************  None *************"
+var = None  # no data
+print "var : ",var
+print type(var)
+print id(var)
