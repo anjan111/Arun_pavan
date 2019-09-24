aaba232b93b531242268681a863a9425a7714f10
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-09-23 19:49:06 -0700

    Add files via upload

---
+++ 000_pythpopn introduction/001_script.py
@@ -0,0 +1,11 @@
+# script mode/ batch mode
+'''
+===>>  new file
+===>>   write the python statement
+===>>    save as .py extension
+===>>   run this  model
+
+'''
+print "hello"
+print "hai"
+print "bye"
